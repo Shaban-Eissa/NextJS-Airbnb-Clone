9343754dc201e824eec1e28105994f308406396e
Shaban-Eissa <shabaneissa767@gmail.com>
2023-06-06 16:15:12 +0300

    NEXT13 Deployed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-    },
-    images: {
-      domains: [
-        'res.cloudinary.com', 
-        'avatars.githubusercontent.com', 
-        'lh3.googleusercontent.com'
-      ]
-    }
+  experimental: {
+    appDir: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
